d08209986f5efcbeffc06605c3fc8c95937be6de
juyoonpark <pjyc17@gmail.com>
2023-11-30 21:34:24 +0900

    makecalculator

---
+++ app.js
@@ -0,0 +1,10 @@
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  powerOf: function (a, b) {
+    console.log(a ** b);
+  },
+};
+calculator.plus(1, 2);
+calculator.powerOf(2, 3);
